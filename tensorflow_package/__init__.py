547346a3dbae40db8b21af384a8e9f652055d67a
mark.ma <mark.ma@qingbao.cn>
2018-09-04 19:56:55 +0800

    python3的部分进行迁移

--- tensorflow_package/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*-coding:utf-8-*-
-"""
-@Time   : 2018/8/27 16:29
-@Author : Mark
-@File   : __init__.py.py
-"""
--- zhuanzhi_package/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*-coding:utf-8-*-
-"""
-@Time   : 2018/9/4 13:58
-@Author : Mark
-@File   : __init__.py.py
-"""
