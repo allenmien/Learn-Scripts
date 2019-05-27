a8dc1799d74a4a2532088b01490a19338a293430
maqi <maqi@xiaoyouzi.com>
2019-05-27 18:28:03 +0800

    merge code

---
+++ bert_package/__init__.py
@@ -0,0 +1,6 @@
+# -*-coding:utf-8-*-
+"""
+@Time   : 2019-03-07 13:52
+@Author : Mark
+@File   : __init__.py.py
+"""
---
+++ spelling_correction/__init__.py
@@ -0,0 +1,5 @@
+'''
+@File   : __init__.py.py
+@Author : Mark
+@Date   : 2019/5/27
+'''
